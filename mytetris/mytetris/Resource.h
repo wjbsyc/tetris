f8ee38cc040ba7cd9f9a3205012f2cae5c299d5c
wjbsyc <wjbsyc@hotmail.com>
2018-06-06 15:28:55 +0800

    init

---
+++ mytetris/mytetris/Resource.h
@@ -0,0 +1,37 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 mytetris.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_WIN32PROJECT2_DIALOG        102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDM_DIFF                        106
+#define IDI_MYTETRIS                    107
+#define IDI_SMALL                       108
+#define IDC_MYTETRIS                    109
+#define ID_dif1                         111
+#define ID_dif2                         112
+#define ID_dif3                         113
+#define ID_LAYOUT1                      114
+#define ID_LAYOUT2                      115
+#define ID_START                        116
+#define ID_STOP                         117
+#define ID_ABOUTME                      118
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ mytetris/mytetris/mytetris.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include "resource.h"
+
+//函数声明
+void checkComplite();  //查看一行是否能消去  采用从上往下的消法，消去一行后把上面的每行都往下移，但是我感觉效率有点低，以后看能不能有点改进
+void drawBlocked(HDC hdc);      //绘制当前已经存在砖块的区域
+void DrawBackGround(HDC hdc);       //绘制背景
+void outPutBoxInt(int num);     //自定义的弹窗函数  用于调试
+void outPutBoxString(TCHAR str[1024]);
+void setRandomT();      //随机生成一个方块用作下一次掉落
+void init_game();       //初始化
+void fillBlock();       //到达底部后填充矩阵
+void RotateTeris(BOOL bTeris[4][4]);        //旋转矩阵
+void DrawTeris(HDC mdc);    //绘制正在下落的方块 
+void drawNext(HDC hdc); //绘制下一个将要掉落的方块
+void drawScore(HDC hdc);    //绘制分数
+void drawCompleteParticle(int line);
+
+int RandomInt(int _min, int _max);       //获取一个随机int
+int CheckValide(int curPosX, int curPosY, BOOL bCurTeris[4][4]);   //给定一个矩阵，查看是否合法
+int selectDiffculty(HWND hWnd, int dif);
+int selectLayOut(HWND hWnd, int layout);
+
+//常量声明
+const int BORDER_X = 5;
+const int BORDER_Y = 5;
+const int SCREEN_LEFT_X = 300 + BORDER_X;
+const int SCREEN_Y = 600 + BORDER_Y;
+const int SCREEN_RIGHT_X = 200 + BORDER_X ;
+const int SCREEN_X = SCREEN_LEFT_X + SCREEN_RIGHT_X;
+const BOOL state_teris[][4][4] =
+{
+	{ { 1,1,1,1 },{ 0,0,0,0 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,1,0 },{ 0,1,1,0 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,1,1 },{ 0,0,0,1 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,1,0 },{ 0,0,1,1 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,0,0 },{ 1,1,1,0 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,1,1 },{ 0,1,0,0 },{ 0,0,0,0 },{ 0,0,0,0 } },
+	{ { 0,1,1,0 },{ 1,1,0,0 },{ 0,0,0,0 },{ 0,0,0,0 } }
+};
+
+
+
+//全局变量声明
+bool g_hasBlocked[50][50];
+RECT rc_left, rc_right, rc_right_top, rc_right_bottom, rc_main;
+int g_speed = 300;
+int t_speed = 300;
+BOOL bCurTeris[4][4];
+BOOL bNextCurTeris[4][4];
+int curPosX, curPosY;
+int rd_seed = 1995421;
+int tPre = 0, tCur;
+int GAME_STATE = 0;
+int GAME_SCORE = 0;
+int GAME_DIFF = 1;
+int NUM_X = 10;
+int NUM_Y = 20;
+int BLOCK_SIZE = 30;
